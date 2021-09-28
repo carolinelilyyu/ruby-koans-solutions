62b770f2115bd46a64352f480d3995176c143b42
Caroline Yu <carolineyu@Carolines-MBP.hsd1.ma.comcast.net>
2021-09-28 13:56:24 -0400

    first commit; up to about_classes

---
+++ about_regular_expressions.rb
@@ -0,0 +1,187 @@
+# -*- coding: utf-8 -*-
+require File.expand_path(File.dirname(__FILE__) + '/neo')
+
+class AboutRegularExpressions < Neo::Koan
+  def test_a_pattern_is_a_regular_expression
+    assert_equal Regexp, /pattern/.class
+  end
+
+  def test_a_regexp_can_search_a_string_for_matching_content
+    assert_equal "match", "some matching content"[/match/]
+  end
+
+  def test_a_failed_match_returns_nil
+    assert_equal nil, "some matching content"[/missing/]
+  end
+
+  # ------------------------------------------------------------------
+
+  def test_question_mark_means_optional
+    assert_equal "ab", "abbcccddddeeeee"[/ab?/]
+    assert_equal "a", "abbcccddddeeeee"[/az?/]
+  end
+
+  def test_plus_means_one_or_more
+    assert_equal "bccc", "abbcccddddeeeee"[/bc+/]
+  end
+
+  def test_asterisk_means_zero_or_more
+    assert_equal "abb", "abbcccddddeeeee"[/ab*/]
+    assert_equal "a", "aabbcccddddeeeee"[/az*/]
+    assert_equal "", "abbcccddddeeeee"[/z*/]
+
+    # THINK ABOUT IT:
+    #
+    # When would * fail to match?
+    #   When it fails to find any occurence of the pattern, it will return a nil.
+  end
+
+  # THINK ABOUT IT:
+  #
+  # We say that the repetition operators above are "greedy."
+  #
+  # Why?
+
+  # Repetition is greedy by default: as many occurrences as possible are matched while still allowing the overall match to succeed. By contrast, lazy matching makes the minimal amount of matches necessary for overall success. A greedy metacharacter can be made lazy by following it with ?.
+  # Extra:
+  # A quantifier followed by + matches possessively: once it has matched it does not backtrack. They behave like greedy quantifiers, but having matched they refuse to “give up” their match even if this jeopardises the overall match.
+
+  # ------------------------------------------------------------------
+
+  def test_the_left_most_match_wins
+    assert_equal "a", "abbccc az"[/az*/]
+  end
+
+  # ------------------------------------------------------------------
+
+  def test_character_classes_give_options_for_a_character
+    animals = ["cat", "bat", "rat", "zat"]
+    # puts animals.select { |a| a[/at/] }
+    #[] makes them options of or with anything following
+    assert_equal ["cat", "bat", "rat"], animals.select { |a| a[/[cbr]at/] }
+
+  end
+
+  def test_slash_d_is_a_shortcut_for_a_digit_character_class
+    #one of more of any of the numbers
+    assert_equal "42", "the number is 42"[/[0123456789]+/]
+    assert_equal nil, "the number is"[/[0123456789]+/]
+    assert_equal "42", "the number is 42"[/\d+/]
+  end
+
+  def test_character_classes_can_include_ranges
+    assert_equal "42", "the number is 42"[/[0-9]+/]
+  end
+
+  def test_slash_s_is_a_shortcut_for_a_whitespace_character_class
+    assert_equal " \t\n", "space: \t\n"[/\s+/]
+  end
+
+  def test_slash_w_is_a_shortcut_for_a_word_character_class
+    # NOTE:  This is more like how a programmer might define a word.
+    #i'm not sure why this isnt "variable_142"
+    assert_equal "variable_1", "variable_1 = 42"[/[a-zA-Z0-9_]+/]
+    assert_equal "variable_1", "variable_1 = 42"[/\w+/]
+  end
+
+  #confused about this 
+  #/./ Any character except a newline. stops after new line?
+  def test_period_is_a_shortcut_for_any_non_newline_character
+    assert_equal "abc", "abc\n123"[/a.+/]
+    assert_equal "abc", "abc\n123b"[/a.+/]
+
+  end
+
+  def test_a_character_class_can_be_negated
+    #non digital character
+    assert_equal "the number is ", "the number is 42"[/[^0-9]+/]
+  end
+
+  def test_shortcut_character_classes_are_negated_with_capitals
+    # A non-digit character ([^0-9])
+    assert_equal "the number is ", "the number is 42"[/\D+/]
+    # A non-whitespace character: /[^ \t\r\n\f\v]/
+    assert_equal "space:", "space: \t\n"[/\S+/]
+    # ... a programmer would most likely do
+    assert_equal " = ", "variable_1 = 42"[/[^a-zA-Z0-9_]+/]
+    #/\W/ - A non-word character ([^a-zA-Z0-9_])
+    assert_equal " = ", "variable_1 = 42"[/\W+/]
+  end
+
+  # ------------------------------------------------------------------
+
+  def test_slash_a_anchors_to_the_start_of_the_string
+    #\A means start of 
+    assert_equal "start", "start end"[/\Astart/]
+    assert_equal nil, "start end"[/\Aend/]
+  end
+
+  def test_slash_z_anchors_to_the_end_of_the_string
+    assert_equal "end", "start end"[/end\z/]
+    assert_equal nil, "start end"[/start\z/]
+  end
+
+  def test_caret_anchors_to_the_start_of_lines
+    #^ means start of and \d means number
+    assert_equal "2", "num 42\n2 lines"[/^\d+/]
+  end
+
+  def test_dollar_sign_anchors_to_the_end_of_lines
+    #$ means at the end of lines \d means any number and + means more than one
+    assert_equal "42", "2 lines\nnum 42"[/\d+$/]
+  end
+
+  def test_slash_b_anchors_to_a_word_boundary
+    #needs to be a new word
+    assert_equal "vines", "bovine vines"[/\bvine./]
+    #only one cuz greedy
+    assert_equal "vinel", "bovine vinel vines"[/\bvine./]
+
+  end
+
+  # ------------------------------------------------------------------
+
+  def test_parentheses_group_contents
+    assert_equal "hahaha", "ahahaha"[/(ha)+/]
+  end
+
+  # ------------------------------------------------------------------
+
+  def test_parentheses_also_capture_matched_content_by_number
+    assert_equal "Gray", "Gray, James"[/(\w+), (\w+)/, 1]
+    assert_equal "James", "Gray, James"[/(\w+), (\w+)/, 2]
+  end
+
+  def test_variables_can_also_be_used_to_access_captures
+    assert_equal "Gray, James", "Name:  Gray, James"[/(\w+), (\w+)/]
+    assert_equal "Gray", $1
+    assert_equal "James", $2
+  end
+
+  # ------------------------------------------------------------------
+
+  def test_a_vertical_pipe_means_or
+    grays = /(James|Dana|Summer) Gray/
+    assert_equal "James Gray", "James Gray"[grays]
+    assert_equal "Summer", "Summer Gray"[grays, 1]
+    assert_equal nil, "Jim Gray"[grays, 1]
+  end
+
+  # THINK ABOUT IT:
+  #
+  # Explain the difference between a character class ([...]) and alternation (|).
+  #Alternations work on entire regex captures while character class only works on characters
+  # ------------------------------------------------------------------
+
+  def test_scan_is_like_find_all
+    assert_equal ["one", "two", "three"], "one two-three".scan(/\w+/)
+  end
+
+  def test_sub_is_like_find_and_replace
+    assert_equal "one t-three", "one two-three".sub(/(t\w*)/) { $1[0, 1] }
+  end
+
+  def test_gsub_is_like_find_and_replace_all
+    assert_equal "one t-t", "one two-three".gsub(/(t\w*)/) { $1[0, 1] }
+  end
+end
